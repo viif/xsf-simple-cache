bfc1278154331a27e5a3a57003fddc5ef8198932
Xie Songfeng <xiesf0601@126.com>
2025-12-15 22:11:06 +0800

    实现有频率老化机制的lfu缓存

--- xsf_lfu_aging_cache.h
+++ xsf_lfu_aging_cache.h
@@ -1,5 +1,5 @@
-#ifndef XSF_LFU_CACHE_H
-#define XSF_LFU_CACHE_H
+#ifndef XSF_LFU_AGING_CACHE_H
+#define XSF_LFU_AGING_CACHE_H
 
 #include <list>
 #include <mutex>
@@ -10,9 +10,13 @@
 namespace xsf_simple_cache {
 
 template <typename K, typename V>
-class XSFLfuCache {
+class XSFLfuAgingCache : public XSFCache<K, V> {
    public:
-    explicit XSFLfuCache(size_t capacity) : capacity_(capacity) {}
+    explicit XSFLfuAgingCache(size_t capacity)
+        : capacity_(capacity), aging_threshold_(10) {}
+
+    XSFLfuAgingCache(size_t capacity, uint32_t aging_threshold)
+        : capacity_(capacity), aging_threshold_(aging_threshold) {}
 
     void put(const K& key, const V& value) override {
         std::lock_guard<std::mutex> lock(mutex_);
@@ -35,6 +39,8 @@ class XSFLfuCache {
             freq2nodes_[1].emplace_back(key, value);
             // 记录 key 到节点的映射
             key2node_[key] = std::prev(freq2nodes_[1].end());
+            // 更新平均频率并进行衰减检查
+            updateAvgFreq(1);
         }
     }
 
@@ -48,6 +54,42 @@ class XSFLfuCache {
     }
 
    private:
+    void updateAvgFreq(uint32_t freq) {
+        avg_freq_ =
+            (avg_freq_ * (key2freq_.size() - 1) + freq) / key2freq_.size();
+        if (avg_freq_ < aging_threshold_) {
+            return;
+        }
+        // 平均频率超过阈值，进行衰减
+        uint32_t decay = aging_threshold_ / 2;
+        // 衰减所有 key 的频率，更新最小频率，计算新的平均频率
+        for (auto& [k, f] : key2freq_) {
+            if (f > decay) {
+                f -= decay;
+                min_freq_ = std::min(min_freq_, f);
+            } else {
+                f = 1;
+                min_freq_ = 1;
+            }
+            avg_freq_ += f;
+        }
+        avg_freq_ /= key2freq_.size();
+        // 重新构建频率到节点、key到节点的映射
+        std::unordered_map<uint32_t, std::list<Node>> new_freq2nodes;
+        std::unordered_map<K, typename std::list<Node>::iterator> new_key2node;
+        for (auto& [k, it] : key2node_) {
+            uint32_t f = key2freq_[k];
+            new_freq2nodes[f].emplace_back(k, it->value);
+            new_key2node[k] = std::prev(new_freq2nodes[f].end());
+        }
+        freq2nodes_ = std::move(new_freq2nodes);
+        key2node_ = std::move(new_key2node);
+        // 重置平均频率
+        for (const auto& [k, f] : key2freq_) {
+            avg_freq_ += f;
+        }
+    }
+
     void increaseFreq(const K& key) {
         // 找到 key 对应的频率
         uint32_t freq = key2freq_[key];
@@ -71,6 +113,8 @@ class XSFLfuCache {
         }
         // 更新 key 到节点的映射
         key2node_[key] = std::prev(new_nodes.end());
+        // 更新平均频率并进行衰减检查
+        updateAvgFreq(freq + 1);
     }
 
     void removeMinFreqKey() {
@@ -98,7 +142,9 @@ class XSFLfuCache {
         Node(const K& k, const V& v) : key(k), value(v) {}
     };
     const size_t capacity_;
+    const uint32_t aging_threshold_{10};
     uint32_t min_freq_{0};
+    uint32_t avg_freq_{0};
     std::mutex mutex_;
 
     std::unordered_map<K, uint32_t> key2freq_;
@@ -108,4 +154,4 @@ class XSFLfuCache {
 
 }  // namespace xsf_simple_cache
 
-#endif  // XSF_LFU_CACHE_H
+#endif  // XSF_LFU_AGING_CACHE_H
